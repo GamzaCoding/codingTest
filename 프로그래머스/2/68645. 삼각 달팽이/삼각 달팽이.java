839302fef8cff7a29209710f68f1de0c848cbb57
GamzaCoding <hyeongseok1995@gmail.com>
2026-03-11 23:46:31 +0900

    [level 2] Title: 삼각 달팽이, Time: 33.87 ms, Memory: 135 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,44 +1,102 @@
 class Solution {
     public int[] solution(int n) {
-        int[] dx = {0, 1, -1};
-        int[] dy = {1, 0, -1};
-        
-        int[][] triangle = new int[n][n];
-        int x = 0;
-        int y = 0;
-        int directionChangeIndex = 0;
-        int value = 1;
+        TriangleBoard board = new TriangleBoard(n);
+        Direction direction = new Direction();
+        Point current = new Point(0, 0);
         
         while(true) {
-            triangle[y][x] = value++;
-            int nextX = x + dx[directionChangeIndex];
-            int nextY = y + dy[directionChangeIndex];
+            board.fill(current);
             
-            // 방향 변화 감지
-            if (nextY == n || nextX == n || nextY == -1 || nextX == -1 || triangle[nextY][nextX] != 0) {
-                directionChangeIndex = (directionChangeIndex + 1) % 3;
-                nextX = x + dx[directionChangeIndex];
-                nextY = y + dy[directionChangeIndex];
-                // 최종 감지
-                if (nextY == n || nextX == n || nextY == -1 || nextX == -1 || triangle[nextY][nextX] != 0) {
+            if (!board.canMove(current, direction)) {
+                direction.changeDirection();
+                if (!board.canMove(current, direction)) {
                     break;
                 }
             }
-            
-            x = nextX;
-            y = nextY;
+            current = current.move(direction);
         }
         
-        int[] result = new int[value-1];
-        int index = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (triangle[i][j] != 0) {
-                    result[index++] = triangle[i][j];
+        return board.flatten();
+    }
+    
+    private class TriangleBoard {
+        private final int[][] board;
+        private int nextValue = 1;
+        
+        public TriangleBoard(int n) {
+            this.board = new int[n][n];
+        }
+        
+        public void fill(Point point) {
+            board[point.getY()][point.getX()] = nextValue++;
+        }
+        
+        public boolean canMove(Point current, Direction direction) {
+            Point next = current.move(direction);
+            int x = next.getX();
+            int y = next.getY();
+            
+            return isInRange(x, y) && board[y][x] == 0;
+        }
+        
+        private boolean isInRange(int x, int y) {
+            return x >= 0 && y >= 0 && x < board.length && y < board.length;
+        }
+        
+        public int[] flatten() {
+            int[] result = new int[nextValue -1];
+            int index = 0;
+            
+            for (int y = 0; y < board.length; y++) {
+                for (int x = 0; x < board.length; x++) {
+                    if (board[y][x] != 0) {
+                        result[index++] = board[y][x];
+                    }
                 }
             }
+            
+            return result;
+        }
+    } 
+    
+    private class Point {
+        private final int x;
+        private final int y;
+        
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Point move(Direction direction) {
+            return new Point(x + direction.getDx(), y + direction.getDy());
         }
         
-        return result;
+        public int getX() {
+            return x;
+        }
+        
+       public int getY() {
+            return y;
+        }
+    }
+    
+    private class Direction {
+        // 아래, 오른쪽, 왼쪽위
+        private int[] dx = {0, 1, -1};
+        private int[] dy = {1, 0, -1};
+        private int directionIndex = 0;
+        
+        public void changeDirection() {
+            directionIndex = (directionIndex + 1) % 3;
+        }
+        
+        public int getDx() {
+            return dx[directionIndex];
+        }
+        
+        public int getDy() {
+            return dy[directionIndex];
+        }
     }
 }
