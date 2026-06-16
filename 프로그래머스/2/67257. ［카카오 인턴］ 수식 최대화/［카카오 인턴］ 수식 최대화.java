68ac6634a4810dc3d8e87e88dcdc4757d47c47c8
GamzaCoding <hyeongseok1995@gmail.com>
2026-06-16 13:42:41 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 1.80 ms, Memory: 81.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+class Solution {
+    private static final String[][] precedences = { 
+        "+-*".split(""),
+        "+*-".split(""),
+        "-+*".split(""),
+        "-*+".split(""),
+        "*+-".split(""),
+        "*-+".split("")
+    };
+    
+    private long calculate(long left, long right, String op) {
+        return switch (op) {
+                case "+" -> left + right;
+                case "-" -> left - right;
+                case "*" -> left * right;
+                default -> 0;
+        };
+    }
+    
+    private long calculate(List<String> tokens, String[] precedence) {
+        for (String op : precedence) {
+            for (int i = 0; i < tokens.size(); i++) {
+                if (tokens.get(i).equals(op)) {
+                    long left = Long.parseLong(tokens.get(i - 1));
+                    long right = Long.parseLong(tokens.get(i + 1));
+                    long result = calculate(left, right, op);
+                    tokens.remove(i - 1);
+                    tokens.remove(i - 1);
+                    tokens.remove(i - 1);
+                    tokens.add(i - 1, String.valueOf(result));
+                    i -= 2;
+                }
+            }
+        }
+        return Long.parseLong(tokens.get(0));
+    }
+    
+    public long solution(String expression) {
+        StringTokenizer st = new StringTokenizer(expression, "+-*", true);
+        List<String> tokens = new ArrayList<>();
+        while (st.hasMoreTokens()) {
+            tokens.add(st.nextToken());
+        }
+        
+        long max = 0;
+        for (String[] precedence : precedences) {
+            long value = Math.abs(calculate(new ArrayList(tokens), precedence));
+            if (value > max) {
+                max = value;
+            }
+        }
+        return max;
+    }
+}
